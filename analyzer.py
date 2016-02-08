905e891c3914801b670bbaad04169bafcf48f253
toshle <rosen_ra4ev@abv.bg>
2016-02-08 22:20:39 +0200

    Basic positive - negative tweet classifier with nltk

---
+++ analyzer.py
@@ -0,0 +1,51 @@
+import nltk
+
+tweets = [
+    (['love', 'this', 'car'], 'positive'),
+    (['this', 'view', 'amazing'], 'positive'),
+    (['feel', 'great', 'this', 'morning'], 'positive'),
+    (['excited', 'about', 'the', 'concert'], 'positive'),
+    (['best', 'friend'], 'positive'),
+    (['not', 'like', 'this', 'car'], 'negative'),
+    (['this', 'view', 'horrible'], 'negative'),
+    (['feel', 'tired', 'this', 'morning'], 'negative'),
+    (['not', 'looking', 'forward', 'the', 'concert'], 'negative'),
+    (['enemy'], 'negative')]
+
+test_tweets = [
+    (['feel', 'happy', 'this', 'morning'], 'positive'),
+    (['larry', 'friend'], 'positive'),
+    (['not', 'like', 'that', 'man'], 'negative'),
+    (['house', 'not', 'great'], 'negative'),
+    (['your', 'song', 'annoying'], 'negative')]
+
+
+def get_words_in_tweets(tweets):
+    all_words = []
+    for (words, sentiment) in tweets:
+      all_words.extend(words)
+    return all_words
+
+def get_word_features(wordlist):
+    wordlist = nltk.FreqDist(wordlist)
+    word_features = wordlist.keys()
+    return word_features
+
+def extract_features(document):
+    document_words = set(document)
+    features = {}
+    for word in word_features:
+        features['contains(%s)' % word] = (word in document_words)
+    return features
+
+word_features = get_word_features(get_words_in_tweets(tweets))
+
+training_set = nltk.classify.apply_features(extract_features, tweets)
+
+classifier = nltk.NaiveBayesClassifier.train(training_set)
+
+#print(classifier.show_most_informative_features(32))
+
+tweet = "I bought a hat that my friends did not like, but I do not car because I like it and this is what matters love love love love love love love"
+
+print(classifier.classify(extract_features(tweet.split())))
